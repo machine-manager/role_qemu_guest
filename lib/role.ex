91d090df71d307e78bccc213b34c26c4afa0c5d0
Ivan Kozik <ivan@ludios.org>
2017-12-25 06:59:54 +0000

    Load 9pnet_virtio module to fix boot-time VirtFS mounting

--- lib/role.ex
+++ lib/role.ex
@@ -1,9 +1,16 @@
 defmodule RoleQemuGuest do
 	def role(_tags \\ []) do
 		%{
-			# We could install qemu-guest-agent here, but apparently we do not need it.
-			# `virsh shutdown VMNAME` can shut down a guest just fine as long as 
-			# HandlePowerKey=poweroff is in /etc/systemd/logind.conf
+			desired_packages: [
+				# We could install qemu-guest-agent here, but apparently we do not need it.
+				# `virsh shutdown VMNAME` can shut down a guest just fine as long as 
+				# HandlePowerKey=poweroff is in /etc/systemd/logind.conf
+			],
+			boot_time_kernel_modules: [
+				# Needed for VirtFS mounting to work at boot time; fixes:
+				# 9pnet: Could not find request transport: virtio
+				"9pnet_virtio"
+			],
 		}
 	end
 end
